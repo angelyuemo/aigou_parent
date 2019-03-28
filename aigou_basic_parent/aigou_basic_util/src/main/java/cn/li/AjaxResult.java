014155f7a2131735e64e989a2b2e93723cd11af4
SpikedLoosestrlfe <1264306831@qq.com>
2019-03-28 10:18:40 +0800

    ss

---
+++ aigou_basic_parent/aigou_basic_util/src/main/java/cn/li/AjaxResult.java
@@ -0,0 +1,54 @@
+package cn.li;
+
+public class AjaxResult {
+    private boolean success = true;
+    private String msg = "操作成功";
+
+    private Object object;//对象值:返回一个对象到前台
+
+
+    public static AjaxResult me(){
+        return new AjaxResult();
+    }
+
+    public boolean isSuccess() {
+        return success;
+    }
+
+    public AjaxResult setSuccess(boolean success){
+        this.success = success;
+        return this;
+    }
+
+    /*public void setSuccess(boolean success) {
+        this.success = success;
+    }*/
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public AjaxResult setMsg(String msg) {
+        this.msg = msg;
+        return this;
+    }
+
+    public Object getObject() {
+        return object;
+    }
+
+    public AjaxResult setObject(Object object) {
+        this.object = object;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "AjaxResult{" +
+                "success=" + success +
+                ", msg='" + msg + '\'' +
+                ", object=" + object +
+                '}';
+    }
+
+}
